167ddbe4b68a15ef8e87f73f0630977c1e5289ee
mr-sebita <sebitaballe@gmail.com>
2020-03-14 18:50:58 -0300

    Aca empiezo a trabajar y lo subo a git

---
+++ app.js
@@ -0,0 +1,64 @@
+// 1. Puesta en común de los tipos de datos que vamos a necesitar
+// para guardar nuestras tareas
+
+// 2. Se arma un array 
+
+// 3. Se hace evidente que los datos tienen que estar afuera
+// Creamos un archivo JSON
+
+// console.log(tareasJson)
+// console.log(typeof tareasJson)
+
+// Salto de Fé
+// Módulos nativos
+// const fs = require('fs');
+// let tareasJson = fs.readFileSync('tareas.json', 'utf-8');
+// let tareas = JSON.parse(tareasJson);
+
+// Micro desafío 1
+// transformar el código anterior en una función
+// function leerJson() {
+//    return JSON.parse(fs.readFileSync('tareas.json', 'utf-8'));;
+// }
+
+// 4. Se lleva el código a un modulo
+
+
+
+/*El tech leader de tu equipo de desarrollo propone refactorizar la app de tareas. Para esto te
+encargamos:
+
+1. Modificar la funcionalidad de listar tareas. Deberás utilizar el método forEach.
+Recordá que forEach puede recibir dos parámetros, siendo el segundo nuestro
+index, es posible que tengas que usarlo.*/
+
+let archivoTareas = require('./tareas');
+
+let accion = process.argv[2];
+
+switch (accion) {
+    case 'listar':
+        console.log('Listado de tareas');
+        let tareas = archivoTareas.leer();
+
+        tareas.forEach(tareas.titulo, tareas.estatus => {
+            console.log(i + '. ' + tareas.titulo + ' - ' + tareas.estado);
+
+        });
+
+
+        console.log()
+        break;
+
+        // Micro desafío 1
+        // Atajar el caso en que no se envíe un parámetro
+    case undefined:
+        console.log('Tenés que pasarme una acción');
+        break;
+
+        // Micro desafío 2
+    default:
+        console.log('No entiendo qué me estás pidiendo');
+        console.log('Las acciones disponibles son: listar');
+        break;
+}
